53999f7e7fd02f0fc68c3a2b150017e0d7d4bad7
Roshan Gautam <Roshan Gautam>
2016-11-04 12:26:09 -0500

    Some code

--- Game_PickFourCards/src/Game_PickFourCards.java
+++ Game_PickFourCards/src/Game_PickFourCards.java
@@ -5,12 +5,30 @@
 public class Game_PickFourCards {
 	public static void main(String[]args){
 		final int deck = 52;
+		int Sum=0;
 		String [] named = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 		System.out.println("The random cards are:");
 		for (int i=0; i<4; i++){
-		int RandomCard = (int)(Math.random()*13);
-		System.out.println(named[RandomCard]);
+			int RandomCard = (int)(Math.random()*13);
+			System.out.println(named[RandomCard]);
+			if (RandomCard==0){
+				Sum++;
+			}
+			else if (RandomCard==10){
+				Sum+=11;
+			}
+			else if (RandomCard==11){
+				Sum+=12;
+			}
+			else if (RandomCard==12){
+				Sum+=13;
+			}
+			else{
+			Sum+=Integer.parseInt(named[RandomCard]);
+			}
+			
 		}
+		System.out.println("The sum is "+ Sum);
 		
 						
 		
