566d37228cae346df57911efc5b012a02518094f
Roshan Gautam <Roshan Gautam>
2016-11-06 17:26:01 -0600

    Finalizing problem1

--- Game_PickFourCards/src/Game_PickFourCards.java
+++ Game_PickFourCards/src/Game_PickFourCards.java
@@ -5,11 +5,14 @@
 public class Game_PickFourCards {
 	public static void main(String[]args){
 		final int deck = 52;
+		// int constant deck
 		int Sum=0;
 		String [] named = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+		// array for storing cards
 		System.out.println("The random cards are:");
 		for (int i=0; i<4; i++){
 			int RandomCard = (int)(Math.random()*13);
+			//getting a random card
 			System.out.println(named[RandomCard]);
 			if (RandomCard==0){
 				Sum++;
@@ -25,14 +28,25 @@ public class Game_PickFourCards {
 			}
 			else{
 			Sum+=Integer.parseInt(named[RandomCard]);
+			// summing the picked cards by converting the string to int type
 			}
 			
 		}
 		System.out.println("The sum is "+ Sum);
-		
+		int num = 0, picks = 0;
+		  while (num != 24) {
+		   num = Card() + Card() + Card() +Card();
+		   //calling the method for picking the card
+		   picks++;
+		 
+		  }
+		 
+		  System.out.println("Number of picks:" + picks*4);
+		 }
+	public static int Card() {
+		  int card = (int) (Math.random() * 52.0); 
+		  return card;
+		 }
 						
 		
 	}
-	
-
-}
